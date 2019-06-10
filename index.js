e7e774f5d7cdb1af6ac44da4799d4471ebf3ea0e
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-10 03:55:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 var katzDeli = [];
 
-function takeANumber(katzDeli, name) {
+// function takeANumber(katzDeli, name) {
 
-  katzDeli.push(`${name}`)
+//   katzDeli.push(`${name}`)
+//   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
   
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+// }
+
+function takeANumber(katzDeli) {
+
+  katzDeli.push(`${number}`)
+  return number++
 }
+  var number = 1
+  
+
+
+
+
+
+
+
 
 function nowServing(katzDeli) {
- let i = 0;
- while (i < katzDeli.length) {
-   i++;
- }
+/* for (let i = 0; i < katzDeli.length; i++) {
+
+ }*/
  if (katzDeli.length === 0) {
   return "There is nobody waiting to be served!";
  }
  else 
-  return (`Currently serving ${katzDeli.shift()}.`);
+  return `Currently serving ${katzDeli.shift()}.`;
 }
+
 
 
 function currentLine(katzDeli) {
@@ -27,10 +42,7 @@ function currentLine(katzDeli) {
   }
   if (katzDeli.length === 0) {
     return "The line is currently empty."
-    
   }
   else 
     return `The line is currently:${line}`
-    
 }
-  
